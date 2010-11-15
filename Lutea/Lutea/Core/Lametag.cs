e8944700500dac1f7302b67bd92fa1e57cd552ff
hiro <hiro@c299f5df-fdf4-43b6-9d36-72601b16bd5e>
2010-11-15 21:13:54 +0000

    LAMETagリーダークラス追加。LAMETagによるギャップレス再生対応。
    git-svn-id: http://lutea.gageas.com/svn/trunk@19 c299f5df-fdf4-43b6-9d36-72601b16bd5e

---
+++ Lutea/Lutea/Core/Lametag.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Gageas.Lutea.Core
+{
+    /// <summary>
+    /// LAMETagを読むクラス
+    /// 最低限の内容だけ。あとで拡張するかも。
+    /// ref. http://gabriel.mp3-tech.org/mp3infotag.html
+    /// </summary>
+    class Lametag
+    {
+        public class LameInfo
+        {
+            public bool isVBR;
+            public int delay;
+            public int padding;
+        }
+        public static LameInfo Read(Stream strm)
+        {
+            byte[] buffer = new byte[10];
+            strm.Read(buffer, 0, 10);
+            if (Encoding.ASCII.GetString(buffer, 0, 3) == "ID3")
+            {
+                var size = (buffer[6] << 21) + (buffer[7] << 14) + (buffer[8] << 7) + buffer[9];
+                strm.Seek(size, SeekOrigin.Current);
+            }
+            buffer = new byte[0x180];
+            strm.Read(buffer, 0, buffer.Length);
+            if (buffer[0] != 0xFF) return null;
+            if (buffer[1] != 0xFB) return null;
+
+            var XingORInfo = Encoding.ASCII.GetString(buffer, 0x24, 4);
+            LameInfo info = new LameInfo();
+            if (XingORInfo == "Xing")
+            {
+                info.isVBR = true;
+            }
+            else if (XingORInfo == "Info")
+            {
+                info.isVBR = false;
+            }
+            else
+            {
+                return null;
+            }
+            info.delay = (buffer[0xb1] << 4) + (buffer[0xb2] >> 4);
+            info.padding = ((buffer[0xb2] & 0x0f) << 8) + buffer[0xb3];
+            return info;
+        }
+    }
+}
