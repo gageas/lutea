c526d3b129c16d47ce371d97b685f1c6fc7d980e
gageas <gageas@gageas.com>
2010-11-23 19:43:40 +0000

    タスクバーまわりの実装(Win7拡張含む)
    git-svn-id: http://lutea.gageas.com/svn/trunk@47 c299f5df-fdf4-43b6-9d36-72601b16bd5e

---
+++ Lutea/DefaultUI/Com.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Gageas.Lutea.DefaultUI
+{
+    class Com
+    {
+        [DllImport("ole32.dll")]
+        public static extern int CLSIDFromString([MarshalAs(UnmanagedType.LPWStr)] string lpsz, out Guid pclsid);
+
+        [DllImport("ole32.dll")]
+        public static extern int CoCreateInstance([MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+            IntPtr pUnkOuter, uint dwClsContext, [MarshalAs(UnmanagedType.LPStruct)] Guid riid, out IntPtr rpv);
+
+        [DllImport("ole32.dll")]
+        public static extern int CoInitialize(IntPtr pvReserved);
+
+        [DllImport("ole32.dll")]
+        public static extern int CoUninitialize();
+
+        public const int CLSCTX_ALL = 0x17;
+    }
+}
