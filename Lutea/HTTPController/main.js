a54633ad7bb1b10d73f787c7f911551af5790064
gageas <gageas@gageas.com>
2010-12-19 19:26:05 +0000

    HTTPControlをComet化 リアルタイム性向上とトラフィック削減ｗ
    git-svn-id: http://lutea.gageas.com/svn/trunk@89 c299f5df-fdf4-43b6-9d36-72601b16bd5e

--- Lutea/HTTPController/main.js
+++ Lutea/HTTPController/main.js
@@ -1,6 +1,12 @@
 ﻿var _file_name = undefined;
 var _playlistQuery = undefined;
+
 var loadDataTimer = null;
+var loadPlaylistTimer = null;
+var commitQueryTimer = null;
+
+var emphasized_element = null;
+
 var Controller = {
 	Play : function(){hitPath("./?mode=control&operation=play");},
 	Stop : function(){hitPath("./?mode=control&operation=stop");},
@@ -14,16 +20,16 @@ var Controller = {
  初期化
 */
 function init() {
-//	setInterval(loadData, 1000);
-	loadData();
+	loadData(false);
+	loadPlaylist(false);
 }
 
 /*
  プレーヤの現在の状態を読み込む
 */
-function loadData() {
+function loadData(useComet) {
     var ajax = new Ajax();
-    ajax.open("GET", "./?mode=xml");
+    ajax.open("GET", "./?mode=xml" + (useComet?"&comet=true":""));
     ajax.onload = function (xmlhttp) {
 		var xml = xmlhttp.responseXML;
 		if(xml == null)return;
@@ -35,6 +41,7 @@ function loadData() {
             }else{
 	            $("infoView").innerHTML = "<small>" + getTagValue(xml,"tagAlbum") + " <sup>#" + getTagValue(xml,"tagTracknumber") + "</sup></small><br>" + 
 	            "<big>" + getTagValue(xml,"tagTitle") + " - " + getTagValue(xml,"tagArtist") + "</big>";
+	            emphasize(file_name);
 			}
 			$("coverArt").src = "./?mode=cover&dummy=" + Math.random();
         }
@@ -44,7 +51,17 @@ function loadData() {
         	loadPlaylist();
         }
         if(loadDataTimer) clearTimeout(loadDataTimer);
-        loadDataTimer = setTimeout(loadData, 1000);
+        var now = new Date();
+		var hours    = now.getHours();    // 時
+		var minutes  = now.getMinutes();  // 分
+		var seconds  = now.getSeconds();  // 秒
+ 
+		if ( hours   < 10 ) hours   = '0' + hours;
+		if ( minutes < 10 ) minutes = '0' + minutes;
+		if ( seconds < 10 ) seconds = '0' + seconds;
+
+        document.title = "Lutea WEB Interface " + hours + ":" + minutes + ":" + seconds + " sync OK.";
+        loadDataTimer = setTimeout(function(){loadData(true);}, 100);
     }
     ajax.send(null);
 }
@@ -52,16 +69,15 @@ function loadData() {
 /*
  playlistを読み込む
 */
-function loadPlaylist(){
+function loadPlaylist(useComet){
     var ajax = new Ajax();
-    ajax.open("GET", "./?mode=xml&type=playlist");
+    ajax.open("GET", "./?mode=xml&type=playlist" + (useComet?"&comet=true":""));
     ajax.onload = function (xmlhttp) {
 		var xml = xmlhttp.responseXML;
 		if(xml == null)return;
 		var items = xml.getElementsByTagName("item");
 
-        var ifr = document.getElementById("playlistViewIframe");
-        var doc = ifr.contentDocument || ifr.contentWindow.document;
+		var doc = getIframeDoc();
         doc.body.innerHTML = "";
 
         var ul = doc.createElement("ul");
@@ -71,6 +87,7 @@ function loadPlaylist(){
         	var li = doc.createElement("li");
         	li.className = "playlistItem";
         	li.innerText = li.textContent = item.getAttribute("tagAlbum") + " - " + item.getAttribute("tagTitle") + "/" + item.getAttribute("tagArtist");
+        	li.id = escape(item.getAttribute("file_name"));
         	var index = item.getAttribute("index");
         	li.onclick = (function(i){
         		return function(){
@@ -79,17 +96,41 @@ function loadPlaylist(){
 	        	}
         	})(i);
         	ul.appendChild(li);
+            if(loadPlaylistTimer) clearTimeout(loadPlaylistTimer);
+	        loadPlaylistTimer = setTimeout(function(){loadPlaylist(true);}, 100);
         }
         doc.body.appendChild(ul);
+        emphasize(_file_name);
     }
     ajax.send(null);
+}
+
+function getIframeDoc(){
+    var ifr = document.getElementById("playlistViewIframe");
+    return ifr.contentDocument || ifr.contentWindow.document;
+}
+
+function emphasize(file_name){
+	if(emphasized_element){
+		emphasized_element.style.fontWeight = "";
+		emphasized_element.style.backgroundColor = "";
+	}
+	emphasized_element = getIframeDoc().getElementById(escape(file_name));
+	if(emphasized_element){
+		emphasized_element.style.backgroundColor = "gold";
+		emphasized_element.style.fontWeight = "bold";
+	}
 }
 
 /*
  createPlaylistを実行する
 */
 function commitQuery(query){
-	hitPath("./?mode=control&operation=createPlaylist&query=" + encodeURIComponent(escape(query)));
+	if(query == "") return; // ""のときはクエリ投げない
+	if(commitQueryTimer) clearTimeout(commitQueryTimer);
+	commitQueryTimer = setTimeout(function(){
+		hitPath("./?mode=control&operation=createPlaylist&query=" + encodeURIComponent(escape(query)));
+	}, 40);
 }
 
 /*
@@ -99,8 +140,6 @@ function hitPath(path){
 	var ajax = new Ajax();
 	ajax.open("GET", path);
 	ajax.send(null);
-    if(loadDataTimer) clearTimeout(loadDataTimer);
-    loadDataTimer = setTimeout(loadData, 400);
 }
 
 /*
@@ -124,14 +163,11 @@ function Ajax(){
 	this.open = function(method,path){xmlhttp.open(method,path);};
 	this.send = function(content){xmlhttp.send(content);};
 	this.onload = function(){};
-	xmlhttp.onload = function(){
-		if(onload_fired)return;
-		onload_fired = true;
-		self.onload(xmlhttp);
-	}
 	xmlhttp.onreadystatechange = function(){
 		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-			xmlhttp.onload();
+			if(onload_fired)return;
+			onload_fired = true;
+			self.onload(xmlhttp);
 		}
 	}
 }
