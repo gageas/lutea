f31d13cb2c382373043edf30e9b22856593af70f
hiro <hiro@c299f5df-fdf4-43b6-9d36-72601b16bd5e>
2013-07-05 17:37:03 +0000

    User32.dll関数の切り出し
    git-svn-id: http://lutea.gageas.com/svn/trunk@367 c299f5df-fdf4-43b6-9d36-72601b16bd5e

---
+++ Lutea/DefaultUI/User32.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Gageas.Lutea.DefaultUI
+{
+    class User32
+    {
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr hIcon);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+    }
+}
