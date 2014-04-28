08786b5b8cabe3c2e967cad89423118ce6c45813
gageas <gageas@gageas.com>
2014-04-28 13:39:40 +0000

    コメントとか整理
    git-svn-id: http://lutea.gageas.com/svn/trunk@530 c299f5df-fdf4-43b6-9d36-72601b16bd5e

--- Lutea/Lutea/NativeWrapper/SQLite3.cs
+++ Lutea/Lutea/NativeWrapper/SQLite3.cs
@@ -5,23 +5,70 @@ using System.Runtime.InteropServices;
 
 namespace Gageas.Wrapper.SQLite3
 {
+    /// <summary>
+    /// SQLite3のラッパ
+    /// </summary>
     public static class SQLite3{
-        public const int SQLITE3_OK = 0; // TODO: あとでenumにするかも？
-        public const int SQLITE3_ROW = 100;
+        /// <summary>
+        /// SQLITE_OKの定数
+        /// </summary>
+        public const int SQLITE_OK = 0;
+
+        /// <summary>
+        /// SQLITE_ROWの定数
+        /// </summary>
+        public const int SQLITE_ROW = 100;
+
+        /// <summary>
+        /// SQLITE_DONEの定数
+        /// </summary>
         public const int SQLITE_DONE = 101;
+
+        /// <summary>
+        /// SQLITE_TRANSIENTの定数
+        /// </summary>
         public const UInt32 SQLITE_TRANSIENT = 0xffffffff;
+
+        /// <summary>
+        /// SQLiteの文字列エンコーディング列挙体
+        /// </summary>
         public enum TextEncoding{
+            /// <summary>
+            /// UTF-8
+            /// </summary>
             SQLITE_UTF8         = 1,
+
+            /// <summary>
+            /// UTF-16(LE)
+            /// </summary>
             SQLITE_UTF16LE      = 2,
+
+            /// <summary>
+            /// UTF-16(BE)
+            /// </summary>
             SQLITE_UTF16BE      = 3,
-            SQLITE_UTF16        = 4,   /* Use native byte order */
-            SQLITE_ANY          = 5,  /* sqlite3_create_function only */
-            SQLITE_UTF16_ALIGNED= 8,   /* sqlite3_create_collation only */
+            
+            /// <summary>
+            /// UTF-16(native byte order)
+            /// </summary>
+            SQLITE_UTF16        = 4,
+
+            /// <summary>
+            /// ANY(sqlite3_create_function only)
+            /// </summary>
+            SQLITE_ANY          = 5,
+
+            /// <summary>
+            /// UTF16 Aligned(sqlite3_create_collation only
+            /// </summary>
+            SQLITE_UTF16_ALIGNED= 8,
         }
     }
 
-    // 所々lock()があるが、SQLite3のDB自体複数スレッドから叩くとよくないらしい（個別にOpenすればよい）のであんまり意味ないか
-    // interruptはその都合上別スレッドから叩いてもいいんじゃないかな
+    /// <summary>
+    /// データベースオブジェクト
+    /// </summary>
+    /// <remarks>原則開いたスレッドと同じスレッドから操作する。interruptは特例？</remarks>
     public class SQLite3DB : IDisposable
     {
         private IntPtr dbPtr = (IntPtr)0;
@@ -36,9 +83,15 @@ namespace Gageas.Wrapper.SQLite3
             return (lpwstr == IntPtr.Zero ? null : Marshal.PtrToStringUni(lpwstr));
         }
 
-        // Exception
+        /// <summary>
+        /// SQLite3の例外クラス
+        /// </summary>
         public class SQLite3Exception : System.ApplicationException
         {
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            /// <param name="message">メッセージ</param>
             public SQLite3Exception(string message)
                 : base(message)
             {
@@ -46,6 +99,10 @@ namespace Gageas.Wrapper.SQLite3
         }
 
         #region Constructor
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="filename">データベースファイル名</param>
         public SQLite3DB(string filename)
         {
             int ret;
@@ -57,13 +114,16 @@ namespace Gageas.Wrapper.SQLite3
             {
                 throw (new SQLite3Exception("db file open error."));
             }
-            if (ret != SQLite3.SQLITE3_OK)
+            if (ret != SQLite3.SQLITE_OK)
             {
                 throw (new SQLite3Exception("db file open error."));
             }
         }
         #endregion
 
+        /// <summary>
+        /// IDisposableの実装
+        /// </summary>
         public void Dispose()
         {
             if (dbPtr != (IntPtr)0)
@@ -74,11 +134,17 @@ namespace Gageas.Wrapper.SQLite3
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
         ~SQLite3DB()
         {
             Dispose();
         }
 
+        /// <summary>
+        /// Extensionの読み込みを有効にする
+        /// </summary>
         public bool EnableLoadExtension
         {
             set
@@ -87,58 +153,87 @@ namespace Gageas.Wrapper.SQLite3
             }
         }
 
+        /// <summary>
+        /// 実行中のクエリを中断する
+        /// </summary>
         public void interrupt()
         {
             sqlite3_interrupt(dbPtr);
         }
 
-        public object[] FetchRow(string table, int rowid) // FIXME: NOT SECURE?
+        /// <summary>
+        /// 行を取得
+        /// </summary>
+        /// <param name="table">テーブル名</param>
+        /// <param name="rowid">行</param>
+        /// <returns>行のデータ</returns>
+        public object[] FetchRow(string table, int rowid)
         {
             object[] ret;
             try
             {
                 using (var stmt = this.Prepare("SELECT * FROM " + table + " WHERE ROWID=?;"))
                 {
+                    if (!stmt.IsReadOnly()) throw new InvalidOperationException();
                     stmt.Bind(1, rowid.ToString());
                     ret = stmt.EvaluateFirstROW();
                 }
             }
             catch (SQLite3Exception)
             {
-                ret = new object[]{};
+                ret = new object[] { };
             }
             return ret;
         }
 
+        /// <summary>
+        /// 行を取得(範囲)
+        /// </summary>
+        /// <param name="table">テーブル名</param>
+        /// <param name="rowid_start">先頭の行ID</param>
+        /// <param name="count">行数</param>
+        /// <param name="collection"></param>
         public void FetchRowRange(string table, int rowid_start, int count, object[][] collection)
         {
             int start = Math.Min(rowid_start, collection.Length);
             int end = Math.Min(rowid_start + count, collection.Length);
-            for (int i = start; i < end; i++)
+            using (var stmt = this.Prepare("SELECT * FROM " + table + " WHERE ROWID=?;"))
             {
-                object[] ret;
-                try
+                if (!stmt.IsReadOnly()) throw new InvalidOperationException();
+                for (int i = start; i < end; i++)
                 {
-                    using (var stmt = this.Prepare("SELECT * FROM " + table + " WHERE ROWID=?;"))
+                    object[] ret;
+                    try
                     {
-                        stmt.Bind(1, (i+1).ToString());
+                        stmt.Reset();
+                        stmt.Bind(1, (i + 1).ToString());
                         ret = stmt.EvaluateFirstROW();
                     }
+                    catch (SQLite3Exception)
+                    {
+                        ret = new object[] { };
+                    }
+                    collection[i] = ret;
                 }
-                catch (SQLite3Exception)
-                {
-                    ret = new object[] { };
-                }
-                collection[i] = ret;
             }
         }
 
-        public void Exec(string sql)
-        {
-            Exec(sql, null);
-        }
+        /// <summary>
+        /// Execのコールバック関数のデリゲート
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="argc"></param>
+        /// <param name="argv"></param>
+        /// <param name="columnNames"></param>
+        /// <returns></returns>
         public delegate int Exec_callback(IntPtr p, int argc, string[] argv, string[] columnNames);
-        public void Exec(string sql, Exec_callback callback)
+
+        /// <summary>
+        /// SQLを実行
+        /// </summary>
+        /// <param name="sql">SQL</param>
+        /// <param name="callback">コールバック関数</param>
+        public void Exec(string sql, Exec_callback callback = null)
         {
             lock (this)
             {
@@ -150,31 +245,73 @@ namespace Gageas.Wrapper.SQLite3
                 finally
                 {
                 }
-                if (ret != SQLite3.SQLITE3_OK)
+                if (ret != SQLite3.SQLITE_OK)
                 {
-                    throw (new SQLite3Exception("exec error."));
+                    throw new SQLite3Exception("exec error.");
                 }
             }
         }
 
+        /// <summary>
+        /// プリペアドステートメントを生成
+        /// </summary>
+        /// <param name="sql">SQL文</param>
+        /// <returns>プリペアドステートメントオブジェクト</returns>
         public STMT Prepare(string sql)
         {
             return new SQLite3DB._STMT(this, sql);
         }
 
-
-        /** STMT 外からSTMTのインスタンスを生成できないように
-         */
+        /// <summary>
+        /// プリペアドステートメントオブジェクト(インタフェース)
+        /// </summary>
         public abstract class STMT : IDisposable
         {
-            public abstract string Source { get; }
+            /// <summary>
+            /// コールバック関数のデリゲート
+            /// </summary>
+            /// <param name="o"></param>
             public delegate void dCallback(Object[] o);
+
+            /// <summary>
+            /// ステートメントを評価する
+            /// </summary>
+            /// <param name="cb"></param>
             public abstract void Evaluate(dCallback cb);
+
+            /// <summary>
+            /// ステートメントを評価し，結果を全て取得
+            /// </summary>
+            /// <returns></returns>
             public abstract object[][] EvaluateAll();
+
+            /// <summary>
+            /// ステートメントを評価し，先頭の行を取得
+            /// </summary>
+            /// <returns></returns>
             public abstract object[] EvaluateFirstROW();
+
+            /// <summary>
+            /// IDisposableの実装
+            /// </summary>
             public abstract void Dispose();
+
+            /// <summary>
+            /// リセット
+            /// </summary>
             public abstract void Reset();
+
+            /// <summary>
+            /// プレースホルダに値をバインド
+            /// </summary>
+            /// <param name="index">プレースホルダのインデックス</param>
+            /// <param name="value">値</param>
             public abstract void Bind(int index,string value);
+
+            /// <summary>
+            /// ステートメントがReadOnlyかどうか
+            /// </summary>
+            /// <returns></returns>
             public abstract bool IsReadOnly();
         }
 
@@ -183,20 +320,18 @@ namespace Gageas.Wrapper.SQLite3
             SQLite3DB db;
             IntPtr stmt;
             int columnCount = -1;
-            string source;
             public _STMT(SQLite3DB db, String sql)
             {
                 this.db = db;
                 int ret = -1;
                 try
                 {
-                    this.source = sql;
                     ret = sqlite3_prepare16(db.dbPtr, sql, sql.Length * 2, out stmt, IntPtr.Zero);
                 }
                 finally
                 {
                 }
-                if (ret != SQLite3.SQLITE3_OK)
+                if (ret != SQLite3.SQLITE_OK)
                 {
                     throw new SQLite3Exception("prepare error\n" + ReadStringFromLPWSTR(sqlite3_errmsg16(db.dbPtr)));
                 }
@@ -216,11 +351,6 @@ namespace Gageas.Wrapper.SQLite3
                 this.Dispose();
             }
 
-            public override string Source
-            {
-                get { return this.source; }
-            }
-
             public override void Reset()
             {
                 sqlite3_reset(this.stmt);
@@ -236,7 +366,7 @@ namespace Gageas.Wrapper.SQLite3
             {
                 int r;
                 if (columnCount == -1) columnCount = sqlite3_column_count(stmt);
-                while (SQLite3.SQLITE3_ROW == (r = sqlite3_step(this.stmt)))
+                while (SQLite3.SQLITE_ROW == (r = sqlite3_step(this.stmt)))
                 {
                     object[] o = new string[columnCount];
                     for (int i = 0; i < columnCount; i++)
@@ -256,7 +386,7 @@ namespace Gageas.Wrapper.SQLite3
                 List<object[]> data = new List<object[]>();
                 int r;
                 if (columnCount == -1) columnCount = sqlite3_column_count(stmt);
-                while (SQLite3.SQLITE3_ROW == (r = sqlite3_step(this.stmt)))
+                while (SQLite3.SQLITE_ROW == (r = sqlite3_step(this.stmt)))
                 {
                     object[] o = new string[columnCount];
                     for (int i = 0; i < columnCount; i++)
@@ -278,7 +408,7 @@ namespace Gageas.Wrapper.SQLite3
                 if (columnCount == -1) columnCount = sqlite3_column_count(stmt);
                 r = sqlite3_step(this.stmt);
                 object[] o = new string[columnCount];
-                if (r == SQLite3.SQLITE3_ROW)
+                if (r == SQLite3.SQLITE_ROW)
                 {
                     for (int i = 0; i < columnCount; i++)
                     {
