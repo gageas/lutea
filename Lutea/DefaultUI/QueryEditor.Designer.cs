706f5e4962c96d30f7b9573dae85572b5d01ae9e
hiro <hiro@c299f5df-fdf4-43b6-9d36-72601b16bd5e>
2014-07-05 17:40:58 +0000

    クエリ入力欄のフォントをMSゴに変更
    git-svn-id: http://lutea.gageas.com/svn/trunk@587 c299f5df-fdf4-43b6-9d36-72601b16bd5e

--- Lutea/DefaultUI/QueryEditor.Designer.cs
+++ Lutea/DefaultUI/QueryEditor.Designer.cs
@@ -40,27 +40,28 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(151, 8);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(301, 22);
+            this.textBox1.Size = new System.Drawing.Size(301, 19);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "新しいクエリ";
             // 
             // textBox2
             // 
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Location = new System.Drawing.Point(4, 19);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(4, 16);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(923, 181);
+            this.textBox2.Size = new System.Drawing.Size(923, 184);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "SELECT * FROM list;";
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(727, 250);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 2;
@@ -72,7 +73,7 @@
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button2.Location = new System.Drawing.Point(836, 250);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 29);
             this.button2.TabIndex = 3;
@@ -86,7 +87,7 @@
             this.label1.Location = new System.Drawing.Point(13, 11);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.Size = new System.Drawing.Size(97, 12);
             this.label1.TabIndex = 4;
             this.label1.Text = "クエリファイルの名前";
             // 
@@ -97,9 +98,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Location = new System.Drawing.Point(16, 39);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(931, 204);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
@@ -118,7 +119,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "QueryEditor";
